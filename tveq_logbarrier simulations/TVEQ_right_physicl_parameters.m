ccd93120d135f4e2331dc8241766ba9a20f1fffc
Dvir Jacobovich <69258459+DvirJacobovich@users.noreply.github.com>
2022-03-31 21:01:09 +0300

    Update TVEQ_right_physicl_parameters.m

--- tveq_logbarrier simulations/TVEQ_right_physicl_parameters.m
+++ tveq_logbarrier simulations/TVEQ_right_physicl_parameters.m
@@ -1,174 +1,177 @@
-    % close all force
-    
-   addpath 'C:\Users\Dvir\Desktop\Doppler Frequencies Mapping'
-   addpath 'C:\Users\Dvir\Desktop\Doppler Frequencies Mapping\Compressive_Sensing_Tools\l1magic\Optimization'
-    addpath '.../'
-      
-    pow = 7;
-    sz = 2^pow;
+% Dvir Jacobovich 2021 - Pr John Howell lab Hebrew University Of Jerusalem
 
-    N = sz^2; 
+addpath('..\')
+% close all force
 
-    % No. of measurements
-    M = round(N/8); 
+addpath 'C:\Users\Dvir\Desktop\Doppler Frequencies Mapping'
+addpath 'C:\Users\Dvir\Desktop\Doppler Frequencies Mapping\Compressive_Sensing_Tools\l1magic\Optimization'
+addpath '.../'
 
-    xs = linspace(-1, 1, sz);
-    ys = xs;
+pow = 7;
+sz = 2^pow;
 
-    [xx, yy] = meshgrid(xs, ys);
+N = sz^2; 
 
-    % PHYSICAL PARAMETERS (Raman-Nath Scattering approx (lamda*L / d^2 << 1): 
+% No. of measurements
+M = round(N/8); 
 
-    % Beams wave lenghts, in [m].
-    lambda = 532e-9; 
+xs = linspace(-1, 1, sz);
+ys = xs;
 
-    % Magnitude of the laser wavevector.
-    k0 = 2*pi / lambda; % in [1/m]
+[xx, yy] = meshgrid(xs, ys);
 
-    % Kerr like Refractive index.
-    n2 = -1.8*10^-4; % [m^2 / mW].
+% PHYSICAL PARAMETERS (Raman-Nath Scattering approx (lamda*L / d^2 << 1): 
 
-    % Fringe spacing.
-    d = 50*10^-6; % [m].
+% Beams wave lenghts, in [m].
+lambda = 532e-9; 
 
-    % Interaction region thickness.
-    L = 9*10^-6; % [m]. 
+% Magnitude of the laser wavevector.
+k0 = 2*pi / lambda; % in [1/m]
 
-    ld = 12*10^-6; % [m].
+% Kerr like Refractive index.
+n2 = -1.8*10^-4; % [m^2 / mW].
 
-    % Grating index wavevector.
-    kg = 2*pi/(110*10^-6); % [1 / m].
+% Fringe spacing.
+d = 50*10^-6; % [m].
 
-    % Liquid crystal relaxation time under these parameters.
-    tau = 72*10^-3; %[s].
+% Interaction region thickness.
+L = 9*10^-6; % [m]. 
 
-    % Gauss signal sqrt(std).
-    w = 0.4;
+ld = 12*10^-6; % [m].
 
-    % Uniformed signal radius.
-    sig_rad = 0.4;
+% Grating index wavevector.
+kg = 2*pi/(110*10^-6); % [1 / m].
 
-    t = 1;
-  
-    % In the future the fD piezo will take place of the balance.
-    % train_data_struct = struct('sample_num', 0, 'balance', zeros(sz, sz),...
-    %       'meaures', zeros(sqrt(M), sqrt(M)), 'piezo', zeros(sz));
-    
-    % Hz frequencies linear range.
-    Hz_freqs = linspace(0.3, 2, 10);
-    v_scale = lambda .* Hz_freqs;
-    % Chosen freq.
-    Hz = Hz_freqs(randi(10));
-    
-    % Rad/sec freq.
-    omega = 2*pi*Hz;
-    
-    % Piezo velocity.
-    v_scale = lambda * Hz;
-    % lambda * Hz = v = A*omega*cos(omega*t) => in t = 0 we have A*omega =
-    % Hz * lambda => A = (Hz * lambda) / omega.
-    
-    % Signal amplitude [V / cm^2].
-    Amp = sqrt((Hz * lambda) / omega); % I added sqrt since we want the 
-    i = 1; % trian_data_len = 1;
+% Liquid crystal relaxation time under these parameters.
+tau = 72*10^-3; %[s].
 
-        signal_type = 1;
+% Gauss signal sqrt(std).
+w = 0.4;
 
-        switch signal_type
-            case 1
-                % Gaussian signal with incertainty of sqrt(w).
-                r = Amp*exp(-((xx.^2) + (yy.^2)) ./ w).*exp(-1i.*pi*t);
+% Uniformed signal radius.
+sig_rad = 0.4;
 
-            case 2
-                % Circular signal radius sig_rad.
-                r = Amp*circle_draw(sz, sig_rad);
-        end
+t = 1;
 
-    Ir = abs(r).^2; 
+% In the future the fD piezo will take place of the balance.
+% train_data_struct = struct('sample_num', 0, 'balance', zeros(sz, sz),...
+%       'meaures', zeros(sqrt(M), sqrt(M)), 'piezo', zeros(sz));
 
-    % Refferenced signal (has no usge now).
-    s = r;
-    Is = abs(s).^2; 
+% Hz frequencies linear range.
+Hz_freqs = linspace(0.3, 2, 10);
+v_scale = lambda .* Hz_freqs;
+% Chosen freq.
+Hz = Hz_freqs(randi(10));
 
-    % Beams wave lenghts, in [m].
-    lambda = 532e-9;
+% Rad/sec freq.
+omega = 2*pi*Hz;
 
-    % Piazo engeen doppler frequencies membren.
-    Hz_rage = linspace(-0.5, 0.5, 10);
-    
-    % Doppler frequencies.
-    piezo_freqs = last_cris_piexo(sz);
-    figure, pcolor(xs, ys, piezo_freqs);
-    title(sprintf('Spacialy varying freqs of %d Piezos [-0.8Hz 0,8Hz] raged', 40));
-    colorbar('eastoutside')
-    
-    % Piazo engeen velocities.
-    piezo_vels = piezo_freqs .* lambda;
-    figure, pcolor(xs, ys, piezo_vels);
-    title(sprintf('Spacialy varying velocities of %d Piezos [m/s]', 40));
-    colorbar('eastoutside')
+% Piezo velocity.
+v_scale = lambda * Hz;
+% lambda * Hz = v = A*omega*cos(omega*t) => in t = 0 we have A*omega =
+% Hz * lambda => A = (Hz * lambda) / omega.
 
-    % PHYSICAL PARAMETERS (Raman-Nath Scattering (lamda*L / d^2 << 1): 
+% Signal amplitude [V / cm^2].
+Amp = sqrt((Hz * lambda) / omega); % I added sqrt since we want the 
+i = 1; % trian_data_len = 1;
 
-    rho = (2*k0*n2.*d ./ (((1 + ld^2*kg^2)^2 + (piezo_freqs.*tau)^2).^0.5)).*(Is.*Ir).^0.5;
-    psi = atan(piezo_freqs.*tau ./ (1 + ld^2*kg^2)^2);
+signal_type = 1;
 
-    % After liquid crystal 
-    I1 = (abs(besselj(0, rho) + 1i.*besselj(1, rho).*exp(-1i.*psi)).^2); % *Ir
-    I0 = (abs(besselj(-1, rho) + 1i.*besselj(0, rho).*exp(-1i.*psi)).^2); % Ir
+switch signal_type
+    case 1
+        % Gaussian signal with incertainty of sqrt(w).
+        r = Amp*exp(-((xx.^2) + (yy.^2)) ./ w).*exp(-1i.*pi*t);
 
-    balance = I1 - I0;
-    figure, image(balance,'CDatamapping','scaled'),set(gca,'FontSize',20);
-    title('Balance signal');
-    colorbar('eastoutside')
-    
-    % We start with one white balance measurement.
-    white_bal_measre = sum(balance, 'all');
+    case 2
+        % Circular signal radius sig_rad.
+        r = Amp*circle_draw(sz, sig_rad);
+end
 
-    permutation1 = randperm(N, M);
-    permutation0 = randperm(N, M);
+Ir = abs(r).^2; 
 
-    % Two different masks wavelets:
+% Refferenced signal (has no usge now).
+s = r;
+Is = abs(s).^2; 
 
-    % Positive Dragon.
-    masks1 = double(dragon_masks(N, ...
-           M, permutation1)');
+% Beams wave lenghts, in [m].
+lambda = 532e-9;
 
-    % Positive Hadamard.
-    masks0 = double(dragon_masks(N, ...
-           M, permutation0)');
+% Piazo engeen doppler frequencies membren.
+Hz_rage = linspace(-0.5, 0.5, 10);
+
+% Doppler frequencies.
+piezo_freqs = deformable_mirror_new(sz);
+figure, pcolor(xs, ys, piezo_freqs);
+title(sprintf('Spacialy varying freqs of %d Piezos [-0.8Hz 0,8Hz] raged', 40));
+colorbar('eastoutside')
+
+% Piazo engeen velocities.
+piezo_vels = piezo_freqs .* lambda;
+figure, pcolor(xs, ys, piezo_vels);
+title(sprintf('Spacialy varying velocities of %d Piezos [m/s]', 40));
+colorbar('eastoutside')
+
+% PHYSICAL PARAMETERS (Raman-Nath Scattering (lamda*L / d^2 << 1): 
+
+rho = (2*k0*n2.*d ./ (((1 + ld^2*kg^2)^2 + (piezo_freqs.*tau)^2).^0.5)).*(Is.*Ir).^0.5;
+psi = atan(piezo_freqs.*tau ./ (1 + ld^2*kg^2)^2);
+
+% After liquid crystal 
+I1 = (abs(besselj(0, rho) + 1i.*besselj(1, rho).*exp(-1i.*psi)).^2); % *Ir
+I0 = (abs(besselj(-1, rho) + 1i.*besselj(0, rho).*exp(-1i.*psi)).^2); % Ir
+
+balance = I1 - I0;
+figure, image(balance,'CDatamapping','scaled'),set(gca,'FontSize',20);
+title('Balance signal');
+colorbar('eastoutside')
+
+% We start with one white balance measurement.
+white_bal_measre = sum(balance, 'all');
+
+permutation1 = randperm(N, M);
+permutation0 = randperm(N, M);
+
+% Two different masks wavelets:
+
+% Positive Dragon.
+masks1 = double(dragon_masks(N, ...
+   M, permutation1)');
+
+% Positive Hadamard.
+masks0 = double(dragon_masks(N, ...
+   M, permutation0)');
 
 
-    b1 = masks1 * reshape(I1.',1,[])';
-    b0 = masks0 * reshape(I0.',1,[])';
+b1 = masks1 * reshape(I1.',1,[])';
+b0 = masks0 * reshape(I0.',1,[])';
 
-    % First balanced projection.
-    b = b1 - b0; 
+% First balanced projection.
+b = b1 - b0; 
 
-    y1 = masks0 * reshape(I1.',1,[])';
-    y0 = masks1 * reshape(I0.',1,[])';
+y1 = masks0 * reshape(I1.',1,[])';
+y0 = masks1 * reshape(I0.',1,[])';
 
-    % Second balanced projection.
-    y = y1 - y0; 
+% Second balanced projection.
+y = y1 - y0; 
 
-    % Subtracting the White Balanced measurment.
-    total_projection = y + b - white_bal_measre;
-    
-    add_noise = 0;
-    switch add_noise 
-        case 1 
-            noise = std(total_projection) * (rand(M,1) - 0.5).* 1e-3;
-            sigma = std(noise);
-            delta = sqrt(M + 2*sqrt(2*M)) * sigma;
-            tot_proj = total_projection + noise;
-    
-        case 0
-            tot_proj = total_projection;
-    end
-   
+% Subtracting the White Balanced measurment.
+total_projection = y + b - white_bal_measre;
 
-    
-   % Final measurement matrix.
+add_noise = 0;
+switch add_noise 
+case 1 
+    noise = std(total_projection) * (rand(M,1) - 0.5).* 1e-3;
+    sigma = std(noise);
+    delta = sqrt(M + 2*sqrt(2*M)) * sigma;
+    tot_proj = total_projection + noise;
+
+case 0
+    tot_proj = total_projection;
+end
+
+
+
+% Final measurement matrix.
 A = masks1 + masks0 - 1;
 At = A';
 
@@ -202,4 +205,4 @@ figure, image(Ip,'CDatamapping','scaled'),set(gca,'FontSize',20);
 title('Reconstruction balanced Per 12.5%');
 colorbar('eastoutside')
 
-    
+
